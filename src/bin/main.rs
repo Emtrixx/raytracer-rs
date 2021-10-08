e6cb81a126cc08cea3fe8f6b357256abe0e9ef05
Jesse Günzl <20597157+Emtrixx@users.noreply.github.com>
2021-10-08 02:48:18 +0200

    add multiple spheres and intersection distance comparison

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,78 +1,80 @@
-use raytracer::{Sphere, Scene, Point, Color, Light, LightKind, render};
+use raytracer::{Sphere, Element, Scene, Color, render};
+use glam::{Vec3};
 
 fn main() {
 
     // Objects
-    let z = 90.;
-    let sphere_red = Sphere {
-        center: Point {
-            x: 0.,
-            y: 2000.,
-            z: 200.,
-        },
-        radius: 2000.,
-        color: Color (255, 0, 0), 
-    };
-    
-    let sphere_green = Sphere {
-        center: Point {
-            x: -100.,
-            y: 45.,
-            z,
-        },
-        radius: 80.,
-        color: Color (0, 255, 0), 
-    };
+    let sphere_red = Element::Sphere( Sphere {
+        center: Vec3::new(0.,0.,-7.5),
+        radius: 1.,
+        color: Color {
+            red: 255.,
+            green: 0.,
+            blue: 0.
+        }, 
+    });
+    let sphere_green = Element::Sphere( Sphere {
+        center: Vec3::new(1.,0.5,-8.6),
+        radius: 1.,
+        color: Color {
+            red: 0.,
+            green: 255.,
+            blue: 0.
+        }, 
+    });
+    let sphere_blue = Element::Sphere( Sphere {
+        center: Vec3::new(-4.,-2.,-7.5),
+        radius: 1.,
+        color: Color {
+            red: 0.,
+            green: 0.,
+            blue: 255.
+        }, 
+    });
+    let elements = vec![sphere_red, sphere_green, sphere_blue];
 
-    let sphere_blue = Sphere {
-        center: Point {
-            x: 100.,
-            y: 45.,
-            z,
-        },
-        radius: 80.,
-        color: Color (0, 0, 255), 
-    };
-
-    let spheres = vec![sphere_red, sphere_green, sphere_blue];
+    // let spheres = vec![sphere_red, sphere_green, sphere_blue];
 
     // Lights
 
-    let ambient = Light {
-        kind: LightKind::Ambient,
-        intensity: 0.2,
-    };
+    // let ambient = Light {
+    //     kind: LightKind::Ambient,
+    //     intensity: 0.2,
+    // };
 
-    let point = Light {
-        kind: LightKind::Point {
-            position: Point {
-                x: 100.,
-                y: -300.,
-                z: 100.,
-            }
-        },
-        intensity: 0.6
-    };
+    // let point = Light {
+    //     kind: LightKind::Point {
+    //         position: Point {
+    //             x: 100.,
+    //             y: -300.,
+    //             z: 100.,
+    //         }
+    //     },
+    //     intensity: 0.6
+    // };
 
-    let directional = Light {
-        kind: LightKind::Directional {
-            direction: Point {
-                x: 100.,
-                y: 300.,
-                z: 400.,
-            }
-        },
-        intensity: 0.4
-    };
+    // let directional = Light {
+    //     kind: LightKind::Directional {
+    //         direction: Point {
+    //             x: 100.,
+    //             y: 300.,
+    //             z: 400.,
+    //         }
+    //     },
+    //     intensity: 0.4
+    // };
 
-    let lights = vec![ambient, directional];
+    // let lights = vec![ambient, directional];
 
     let scene = Scene {
-        width: 1920,
-        height: 1080,
-        spheres: spheres,
-        lights: lights,
+        width: 800,
+        height: 600,
+        elements,
+        fov: 60.
+        // lights: lights,
     };
 
-    render(&scene);
+    let img = render(&scene);
+
+    img.save("test.png").unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,48 @@
 use image::{DynamicImage, GenericImage};
 use glam::{Vec3};
 
+#[derive(Debug, Clone, Copy)]
 pub struct Color {
     pub red: f32,
     pub green: f32,
     pub blue: f32,
 }
 
-pub trait Intersectable {
-    fn intersect(&self, ray: &Ray) -> bool;
+
+impl Color {
+    pub fn to_rgba(&self) -> image::Rgba<u8> {
+        image::Rgba::<u8>([self.red as u8,self.green as u8,self.blue as u8,0])
+    }
 }
 
+pub trait Intersectable {
+    fn intersect(&self, ray: &Ray) -> Option<f32>;
+}
+
+pub enum Element {
+    Sphere(Sphere),
+    Plane(Plane),
+}
+
+impl Intersectable for Element {
+    fn intersect(&self ,ray: &Ray) -> Option<f32> {
+        match *self {
+            Element::Sphere(ref s) => s.intersect(ray),
+            Element::Plane(ref p) => p.intersect(ray),
+        }
+    }
+}
+
+impl Element {
+    pub fn color(&self) -> Color {
+        match *self {
+            Element::Sphere(ref s) => s.color,
+            Element::Plane(ref p) => p.color,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub struct Sphere {
     pub center: Vec3,
     pub radius: f32,
@@ -18,24 +50,72 @@ pub struct Sphere {
 }
 
 impl Intersectable for Sphere {
-    fn intersect(&self, ray: &Ray) -> bool {
+    fn intersect(&self, ray: &Ray) -> Option<f32> {
         //from origin to center of sphere
         let line_to_center = self.center - ray.origin;
-        let ray_section_length_squared = line_to_center.dot(ray.direction);
+        let ray_section_length = line_to_center.dot(ray.direction);
         //Faster than standard Pythagoras: h**2 - adj**2
-        let connection_squared = line_to_center.dot(line_to_center) - ray_section_length_squared;
+        let connection_squared = line_to_center.dot(line_to_center) - ray_section_length * ray_section_length;
+        let radius_squared = self.radius * self.radius;
 
-        connection_squared < (self.radius * self.radius)
+        if connection_squared > radius_squared {
+            return None;
+        }
+        
+        let thic = ((self.radius * self.radius) - connection_squared).sqrt();
+        
+        // let connection = connection_squared.sqrt();
 
+        // let right_angle_point = self.center + connection;
+
+        let distance_intersection1 = ray_section_length - thic;
+        let distance_intersection2 = ray_section_length + thic;
+
+        if distance_intersection1 < 0. && distance_intersection2 < 0. {
+            return None;
+        };
+
+        let dist = if distance_intersection1 < distance_intersection2 { distance_intersection1 } else { distance_intersection2 };
+        Some(dist)
     }
 }
+
+pub struct Plane {
+    pub point: Vec3,
+    pub normal: Vec3,
+    pub color: Color
+}
+
+impl Intersectable for Plane {
+    fn intersect(&self, ray: &Ray) -> Option<f32> {
+        Some(3.)
+    }
+}
+
+// FROM TUTORIAL MAYBE USELESS
+// pub struct Intersection<'a> {
+//     pub distance: f32,
+//     pub object: &'a Sphere,
+// }
+
+// impl<'a> Intersection<'a> {
+//     pub fn new<'b>(distance: f32, object: &'b Sphere) -> Intersection<'b> {
+//         //
+//     }
+// }
+// impl Scene {
+//     pub fn trace(&self, ray: &Ray) -> Option<Intersection> {
+//         self.spheres
+//     }
+// }
 
 pub struct Scene {
     pub width: u32,
     pub height: u32,
     pub fov: f32,
-    pub sphere: Sphere
+    pub elements: Vec<Element>,
 }
+
 
 pub struct Ray {
     pub origin: Vec3,
@@ -44,11 +124,11 @@ pub struct Ray {
 
 impl Ray {
     pub fn create_prime(x: u32, y: u32, scene: &Scene) -> Ray {
-        let fov_adjustement = to_radians(scene.fov)
+        let fov_adjustement = (to_radians(scene.fov) / 2.).tan() ;
         let aspect_ratio = (scene.width as f32) / (scene.height as f32);
-        let viewport_x = (((x as f32 + 0.5) / scene.width as f32) * 2.0 - 1.0) * aspect_ratio;
-        let viewport_y = 1.0 - ((y as f32 + 0.5) / scene.height as f32) * 2.0;
-
+        let viewport_x = ((((x as f32 + 0.5) / scene.width as f32) * 2.0 - 1.0) * aspect_ratio) * fov_adjustement;
+        let viewport_y = (1.0 - ((y as f32 + 0.5) / scene.height as f32) * 2.0) * fov_adjustement;
+        
         Ray {
             origin: Vec3::ZERO, 
             direction: Vec3::new(viewport_x, viewport_y, -1.0).normalize(),
@@ -57,7 +137,27 @@ impl Ray {
 }
 
 pub fn render(scene: &Scene) -> DynamicImage {
-    DynamicImage::new_rgb8(scene.width, scene.height)
+    let mut image = DynamicImage::new_rgb8(scene.width, scene.height);
+    let black = image::Rgba::<u8>([0,0,0,0]);
+
+    for x in 0..scene.width {
+        for y in 0..scene.height {
+            //standard pixel black
+            image.put_pixel(x, y, black);
+            let ray = Ray::create_prime(x, y, &scene);
+
+            let mut nearest = f32::INFINITY;
+            for sphere in &scene.elements {
+                if let Some(dist) = sphere.intersect(&ray) {
+                    if dist < nearest {
+                        nearest = dist;
+                        image.put_pixel(x, y, sphere.color().to_rgba());
+                    }
+                } 
+            }
+        };
+    };
+    image
 }
 
 pub fn to_radians(x: f32) -> f32 {
